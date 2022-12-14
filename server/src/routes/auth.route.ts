ec034630c119b62688fd447a529fd602a6cf66da
Bryan Avila <bryanjimavila@gmail.com>
2022-12-15 06:29:35 +0800

    add test routes for auth guard

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { refreshToken, signInUser, signUpUser } from "../controllers/auth.controller";
+import { refreshToken, signInUser, signOutUser, signUpUser } from "../controllers/auth.controller";
+import { authGuard } from "../middleware/auth.guard";
 
 export const authRouter = Router()
 
 authRouter.post('/login', signInUser)
 authRouter.post('/register', signUpUser)
 authRouter.post('/refresh', refreshToken)
+authRouter.post('/logout', signOutUser)
+
+
+authRouter.post('/test', authGuard, (req, res) => {
+    res.send("Test");
+})
