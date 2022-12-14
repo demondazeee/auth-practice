540049086be72d269b782dbf3d7ec125a90ffddb
Bryan Avila <bryanjimavila@gmail.com>
2022-12-15 06:29:05 +0800

    update error response

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createUser, loginUser, refresh_token } from "../services/auth.service";
+import { createUser, loginUser, logoutUser, refresh_token } from "../services/auth.service";
 
 export const signUpUser = async (req: Request, res: Response) => {
     const {username, password} = req.body
@@ -13,7 +13,14 @@ export const signInUser = async (req: Request, res: Response) => {
     res.status(200).send(user)
 }
 
+export const signOutUser = async (req: Request, res: Response) => {
+    const user = logoutUser(res)
+    res.status(200).send({
+        message: "user logged out"
+    })
+}
+
 export const refreshToken = async (req: Request, res: Response) => {
     const user = await refresh_token(req, res)
-    res.status(200).send(user)
+    return user
 }
