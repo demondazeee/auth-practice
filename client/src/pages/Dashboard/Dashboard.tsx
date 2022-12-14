ec961d742e81cdaf07ed6de979bebaef8015fbf0
Bryan Avila <bryanjimavila@gmail.com>
2022-12-15 06:31:43 +0800

    add dashboard functionality

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,23 +1,37 @@
 import styled from "styled-components"
-import React from 'react'
+import React, { useContext } from 'react'
 import {Container} from '../../components/Layouts/Container'
 import { H2 } from "../../components/Elements/Headings"
+import { PrimaryButton } from "../../components/Elements/Buttons"
+import { authContext } from "../../store/AuthContext"
 
 
-const DashboardMain = styled.main``
+const DashboardMain = styled.main`
+`
 
-const DashboardContainer = styled.div``
+const DashboardContainer = styled.div`
+    margin-top: 100px;
+    display: flex;
+    justify-content: space-around;
+`
 
 const Dashboard = () =>{
+    const {user,logoutUser} = useContext(authContext)
+
     return (
         <>
             <DashboardMain>
                 <Container>
                     <DashboardContainer>
-                        <H2>Welcome!</H2>
+                        <H2>Welcome! {}</H2>
+                        <PrimaryButton onClick={()=>{
+                            logoutUser()
+                        }}>Logout</PrimaryButton>
                     </DashboardContainer>
                 </Container>
             </DashboardMain>
         </>
     )
 }
+
+export default Dashboard
